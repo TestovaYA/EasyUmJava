e1da3cf91bb6f5423cb5dc509b5281a6c1ced736
Yuliya Testova <yuliya.testova@ural.rt.ru>
2024-07-11 22:19:33 +0500

    add guava

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import com.google.common.math.IntMath;
+import java.math.RoundingMode;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+
+        int number = IntMath.divide(10, 3, RoundingMode.UP);
+        System.out.println(number);
     }
 }
